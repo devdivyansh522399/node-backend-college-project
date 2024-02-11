5091a4f2703421b189ad44803375d1912ffa99b6
devdivyansh522399 <divyanshdev87070@gmail.com>
2024-02-11 20:36:45 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+// Import required modules
+const express = require('express');
+
+
+// Create an Express application
+const app = express();
+
+// Define routes
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
